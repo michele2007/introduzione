65d9618e38221568dbe3990d79a44423c7758c56
michele2007 <michelerecchimurzi0@gmail.com>
2024-09-23 11:13:43 +0200

    intrroduzione(!)

---
+++ static/script.js
@@ -0,0 +1,15 @@
+//codice js 
+
+let variabile = prompt("come ti chiami?")
+
+//prompt è una funzione che chiede un'input e da un'output
+
+//alert mostra un pop up nel browser 
+alert("benvenuto " + variabile)
+
+console.log("benvenuto " + variabile) // logga in console il messaggio passato come alert
+
+if(variabile === null) {
+
+
+}
